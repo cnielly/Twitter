50143d34ad450b24e0684d9f30f2511c55caffd4
Benoit Audigier <benoit.audigier@polytechnique.edu>
2017-10-19 13:54:43 +0200

    add gettweets

--- generalFunctions.R
+++ generalFunctions.R
@@ -14,3 +14,26 @@
 #' }
 treatTweet <- function(x) {tolower(str_replace_all(x,"[^[:graph:]]", " "))}
 
+#' To retrieve the tweets
+#'
+#' @param searchWords the words to search
+#' @param language fr, en
+#'
+#' @return a list of tweets
+#' @export
+#'
+#' @examples
+getTweets <- function(searchWords = c("RedStarFC"), language = "fr"){
+  
+  tweets <- c()
+  
+  # Retrieving the tweets
+  for (w in words){
+    tweets = c(tweets, searchTwitter(w, n=1000, lang=language))
+  }
+  
+  # Treating the data
+  tweets = sapply(tweets, function(x) treatTweets(x$getText())) # To treat the tweets
+  
+  return tweets
+}
