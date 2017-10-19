8ad018b34b49136ff6cbca6061f6fd503d9b9375
Benoit Audigier <benoit.audigier@polytechnique.edu>
2017-10-19 13:40:58 +0200

    add a function

--- generalFunctions.R
+++ generalFunctions.R
@@ -1,1 +1,16 @@
 ### The functions used by the other files
+
+
+#' To treat the tweets
+#'
+#' @param x a string
+#'
+#' @return a string
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' treatTweets("The new iPhone sucks")
+#' }
+treatTweets <- function(x) {tolower(str_replace_all(x,"[^[:graph:]]", " "))}
+
