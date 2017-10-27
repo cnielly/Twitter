e84def08f62fb7df16e7a67c65db1352faa3e28f
cnielly <cyprien78@gmail.com>
2017-10-27 15:36:53 +0200

    tancheries

--- drawTwitterHeatMap.R
+++ drawTwitterHeatMap.R
@@ -1,18 +1,21 @@
 ### http://www.r-graph-gallery.com/79-levelplot-with-ggplot2/
 ### http://www.r-graph-gallery.com/portfolio/heatmap/
 
-source("generalFunctions.R")
+#source("generalFunctions.R")
+library(lubridate)
 
 ### Example of grid
 # x <- seq(1,10, length.out=20)
 # y <- seq(1,10, length.out=20)
 # data <- expand.grid(X=x, Y=y)
+# 
 # data$Z <- runif(400, 0, 5)
-
+# data$Z[1][1] = 100
+# drawHeatMap(x, y, data)
 
 
 # https://stackoverflow.com/questions/21376176/counts-for-time-intervals
-tweets <- getTweets(getOnlyText = FALSE)
+#tweets <- getTweets(getOnlyText = FALSE)
 t <- tweets %>% select(created)
 # table(format(chron::as.chron(t$created), "%d %"))
 # table(format(as.Date(t$created), "%d %b"))
@@ -20,18 +23,13 @@ halfHours <- ifelse(minute(t$created) < 30, 0, 30)
 t$datesHH <- t$created
 minute(t$datesHH) <- halfHours; second(t$datesHH) <- 0
 sortedHH <- data.frame(table(t[ ,2]))
-begin <- test$Var1[1]
-end <- test$Var1[length(test$Var1)]
+begin <- sortedHH$Var1[1]
+# end <- test$Var1[length(test$Var1)]
 
 time <- seq(chron::as.chron(as.character(begin)), chron::as.chron(as.character(end)), by = 0.02083333333333333333) # unit is a day
-values <- integer(length(time))
+# values <- integer(length(time))
 
 ### todo : code the x, y and z for heatmap with values on sortedHH
-for(i in 1:length(values)){
-  if((sortedHH %>% filter(Var1 == time[i])  )
-}
-
-
 
 
 drawHeatMap <- function(x, y, z){
@@ -39,5 +37,6 @@ drawHeatMap <- function(x, y, z){
     theme_bw() + 
     scale_fill_gradient(low="white", high="blue") 
 }
+
 
 
