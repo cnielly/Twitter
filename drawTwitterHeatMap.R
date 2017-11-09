28950a52e4e58250949de10a18536ca655f06036
cnielly <cyprien78@gmail.com>
2017-11-09 14:35:55 +0100

    Clean file to plot the number of tweets

--- drawTwitterHeatMap.R
+++ drawTwitterHeatMap.R
@@ -3,27 +3,38 @@ library(lubridate)
 library(ggplot2)
 library(dplyr)
 
+#' Plot the number of tweets during the day
+#'
+#' @param tweets 
+#'
+#' @return nothing
+#' @export
+#'
+#' @examples
+#' 
+drawTweetFreq <- function(tweets){
 
-#tweets <- getTweets(getOnlyText = FALSE)
-halfHours <- ifelse(minute(t$created) < 30, 0, 30)
-t$datesHH <- t$created
-minute(t$datesHH) <- halfHours; second(t$datesHH) <- 0
-sortedHH <- data.frame(table(t[ ,2]))
-
-#Création de nouvelles colonnes
-sortedHH <- sortedHH %>% mutate(date_time = ymd_hms(Var1))
-sortedHH <- sortedHH %>% mutate(day = as.Date(date_time))
-#sortedHH <- sortedHH %>% mutate(hour = strsplit(as.character(date_time), " "))
-sortedHH <- sortedHH %>% mutate(hourGoodFormat = format(date_time, "%H:%M"))
-# sortedHH <- sortedHH %>% mutate(hour = tail(hour[[]][2]))
-#for (i in seq(1,length(sortedHH$hour)))
- # sortedHH$hour[[i]] <- sortedHH$hour[[i]][2]
-
-#Dessin Gaph
-
-
-p <- ggplot(sortedHH, aes(x = hourGoodFormat, y = Freq)) +
-  geom_point() +
-  theme(axis.text.x=element_text(angle=90)) +
-  scale_x_discrete(breaks = c("00:00","02:00", "04:00", "06:00","08:00","10:00","12:00","14:00","16:00","18:00","20:00","22:00"))
-p
+  t <- tweets %>% select(created)
+  #We are going to work on one specific column of the tweets dataframe: when the tweets were released ("created")
+  halfHours <- ifelse(minute(t$created) < 30, 0, 30)
+  minute(t$created) <- halfHours
+  second(t$created) <- 0
+  #Sort the created values in groups of 30 minutes
+  sortedHH <- data.frame(table(t[1]))
+  
+  sortedHH <- sortedHH %>% mutate(date_time = ymd_hms(Var1))
+  sortedHH <- sortedHH %>% mutate(day = as.Date(date_time))
+  sortedHH <- sortedHH %>% mutate(hour = format(date_time, "%H:%M"))
+  #Creation of two new columns for the plot : "date" and "hour"
+  
+  p <- ggplot(sortedHH, aes(x = hour, y = Freq)) +
+    geom_point() +
+    xlab("Hour of the day") +
+    ylab("Nb of tweets") +
+    ggtitle("Number of tweets during a day") +
+    theme(axis.text.x=element_text(angle=90)) +
+    scale_x_discrete(breaks = c("00:00","02:00", "04:00", "06:00","08:00","10:00","12:00","14:00","16:00","18:00","20:00","22:00"))
+  
+  p
+  #Plotting part
+}
